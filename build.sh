f6ddd4a79791c2a95da1f1b0644fcddf526a255e
Wanaagconer <Wanaagcorner@gmail.com>
2026-05-14 14:36:09 +0300

    build.sh: création automatique du superadmin au déploiement

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Créer le superadmin automatiquement s'il n'existe pas
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@mhechange.dj', 'pastinio2023ZES')
+    print('Superuser créé.')
+else:
+    print('Superuser existe déjà.')
+"
